6661553442032e259622c020813b29003fcf6664
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-02 20:05:54 +0100

    chore(deps): add fonts Inter, Sora, PlayfairDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ export function App() {
   return (
     <section>
       <header>
-        <h1 className="font-semibold text-xl text-stone-500">Tanstack Form</h1>
-        <p className="text-lg text-gray-500">
+        <h1 className="font-playfair font-semibold text-xl text-stone-500">
+          Tanstack Form
+        </h1>
+        <p className="font-inter text-lg text-gray-500">
           lets see what tanner is on about again
         </p>
       </header>
