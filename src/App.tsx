00f2e865d0857733345513142ca39d29f39522d0
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-02 20:03:01 +0100

    chore(setup): tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ export function App() {
   return (
     <section>
       <header>
-        <h1>Tanstack Form</h1>
-        <p>lets see what tanner is on about again</p>
+        <h1 className="font-semibold text-xl text-stone-500">Tanstack Form</h1>
+        <p className="text-lg text-gray-500">
+          lets see what tanner is on about again
+        </p>
       </header>
     </section>
   );
