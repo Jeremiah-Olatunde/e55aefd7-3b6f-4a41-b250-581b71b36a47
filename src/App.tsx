5eed8fee42624f03404764b1073562ce8b29fb19
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-03 22:29:35 +0100

    feat: add cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -286,7 +286,7 @@ function FormFieldInput({
         w-full rounded-md px-2 py-2 font-medium text-stone-500 
         placeholder:font-medium placeholder:text-stone-300 
         focus:outline-none
-        ${invalid ? "border-red-500  focus:border-red-500" : "border-stone-200  focus:border-stone-200"}
+        ${invalid ? "border-red-300  focus:border-red-400" : "border-stone-200  focus:border-stone-200"}
       `}
     />
   );
